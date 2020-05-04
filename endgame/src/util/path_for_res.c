6597323b8555291f67a0bda241108aa1bcc45094
Rostyslav Druzhchenko <rdruzhchenko@teamworkcommerce.com>
2020-05-04 19:39:45 +0300

    Add missed file.

---
+++ endgame/src/util/path_for_res.c
@@ -0,0 +1,28 @@
+//
+// Created by Rostyslav Druzhchenko on 04.05.2020.
+//
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+void path_for_res(const char *file_name, char *path) {
+#ifdef CMAKE_WIN
+    strcpy(path, "../resources/");
+    strcat(path, file_name);
+#else
+    char *paths[] = {
+            "../MUP/endgame/resources/",
+            "../resources/",
+            "./resources/" };
+    for (int i = 0; i < 3; i++) {
+        strcpy(path, paths[i]);
+        strcat(path, file_name);
+        int fd = open(paths[i], O_RDONLY);
+        if (fd != -1) {
+            close(fd);
+            return;
+        }
+    }
+#endif
+}
