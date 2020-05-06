2fc28c1fb56d3700180e96c31b0d9fc140e59888
Rostyslav Druzhchenko <rdruzhchenko@teamworkcommerce.com>
2020-05-06 09:01:57 +0300

    Change formatting and move files

--- endgame/src/about.c
+++ endgame/src/about.c
@@ -1,15 +1,15 @@
 #include "header.h"
 
 int show_about(SDL_Renderer *renderer) {
-	SDL_Rect about_rect = {0, 0, WINDOW_WIDTH, WINDOW_HEIGHT};
+    SDL_Rect about_rect = {0, 0, WINDOW_WIDTH, WINDOW_HEIGHT};
     SDL_Texture *img_about = IMG_LoadTexture(renderer, MX_RES("about.png"));
 
     SDL_Event event;
     while (1) {
-    	SDL_RenderCopy(renderer, img_about, NULL, &about_rect);
-    	while(SDL_PollEvent(&event)) {
+        SDL_RenderCopy(renderer, img_about, NULL, &about_rect);
+        while (SDL_PollEvent(&event)) {
             if (event.type == SDL_KEYUP) {
-                if(event.key.keysym.sym == SDLK_ESCAPE) {
+                if (event.key.keysym.sym == SDLK_ESCAPE) {
                     return 6;
                 }
             }
---
+++ endgame/src/scene/game/print_note.c
@@ -0,0 +1,15 @@
+#include "header.h"
+
+void print_notes(App *app, t_notes *note) {
+    if (note->hp > 0) {
+        SDL_RenderCopy(app->renderer, note->nota.textr, NULL, &note->nota.n_1);
+        SDL_RenderCopy(app->renderer, note->nenota.textr, NULL,
+                       &note->nenota.n_1);
+        SDL_RenderCopy(app->renderer, note->nota.textr, NULL, &note->nota.n_2);
+        SDL_RenderCopy(app->renderer, note->nenota.textr, NULL,
+                       &note->nenota.n_2);
+        SDL_RenderCopy(app->renderer, note->nota.textr, NULL, &note->nota.n_3);
+        SDL_RenderCopy(app->renderer, note->nenota.textr, NULL,
+                       &note->nenota.n_3);
+    }
+}
